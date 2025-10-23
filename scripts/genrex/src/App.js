ccdb3ce590ec36e1379a68b0b202cc480a896818
ishan-j <ishanjoshi1001@gmail.com>
2025-10-23 20:25:51 +0530

    comm 60

--- scripts/genrex/src/App.js
+++ scripts/genrex/src/App.js
@@ -6,10 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello everyone</h1>
-	<p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
       </header>
     </div>
   );
