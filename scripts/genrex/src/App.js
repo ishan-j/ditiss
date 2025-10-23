3559ddb5cd7b9a630d42096e4e9e3f2dfef34ca7
ishan-j <ishanjoshi1001@gmail.com>
2025-10-23 20:25:26 +0530

    comm 52

--- scripts/genrex/src/App.js
+++ scripts/genrex/src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello everyone</h1>
 	<p>
           Edit <code>src/App.js</code> and save to reload.
