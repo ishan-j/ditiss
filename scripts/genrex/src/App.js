ac2f14f39b2c3a71dd30e31f573fb4df1d7b78f4
ishan-j <ishanjoshi1001@gmail.com>
2025-10-23 20:26:44 +0530

    comm 78

--- scripts/genrex/src/App.js
+++ scripts/genrex/src/App.js
@@ -2,11 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello everyone</h1>
-      </header>
+        <h1>Hello everyone</h1>	     
+ </header>
     </div>
   );
 }
